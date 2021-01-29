eb808a52f9139cc0be1737713f1140d00c51fb0f
luis fernando machado <luisfernandomgrs@gmail.com>
2021-01-28 22:38:44 -0300

    initial code javascript

--- scripts.js
+++ scripts.js
@@ -26,36 +26,84 @@ const transactions = [
         date: '23/01/2021'
     },
     {
-        id: 1,
+        id: 3,
         description: 'Internet',
         amount: 20000,
         date: '23/01/2021'
+    },
+    {
+        id: 4,
+        description: 'Serviços',
+        amount: 50000,
+        date: '28/01/2021'
     }
 ]
 
 const Transaction = {
+
     incomes() {
-        //  somar todas as entradas
+        let income = 0;
+        transactions.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount;
+            }
+        })
+        return income;
     },
     expenses() {
-        //  somar as saídas
+        let expense = 0;
+        transactions.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount;
+            }
+        })
+        return expense;
     },
     total() {
-        //  entradas - saídas
+        return "Discover"
     }
 }
 
 const DOM = {
-    innerHTMLTransaction(){
+    addTransaction(ItemTransaction, index) {
+        console.log(ItemTransaction)
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(ItemTransaction)
+
+        // DOM.transactionsContainer
+
+        // console.log(tr.innerHTML)
+    },
+    innerHTMLTransaction(tagItem){
         const html = `
-        <tr>
-            <td class="description">Luz</td>
-            <td class="expense">- R$ 500,00</td>
-            <td class='date'>23/01/2021</td>
+            <td class="description">${tagItem.description}</td>
+            <td class="expense">${tagItem.amount}</td>
+            <td class='date'>${tagItem.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover Transação">
             </td>
-        </tr>
         `
+
+        return html
     }
 }
+
+// DOM.addTransaction(transactions[2])
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g,"")
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
